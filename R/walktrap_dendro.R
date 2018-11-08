5eb55bdf20f36b82eb73f2455fd21a02fac72c31
Kevin Cazelles <kcazelle@uoguelph.ca>
2018-11-08 11:26:08 -0500

    small edits on fig2ab

--- R/walktrap_dendro.R
+++ R/walktrap_dendro.R
@@ -15,39 +15,40 @@
 #' @importFrom graphicsutils darken encircle plot0
 #' @export
 
-walktrap_dendro <- function(links, nodes, vec_col1 = c("#f1bf5f", "#00ac89", "#27ade3"), 
+walktrap_dendro <- function(links, nodes, vec_col1 = c("#f1bf5f", "#00ac89", "#27ade3"),
     vec_col2 = c("#d72968", "#a18037", "#5523b4", "#063632", 1)) {
-    
+
     nodes$idnum <- as.numeric(gsub(nodes$id, pattern = "\\D", replacement = ""))
-    
-    ################################## 
+
+    ##################################
     net <- igraph::graph.data.frame(links, nodes, directed = F)
     gcon <- igraph::simplify(net, edge.attr.comb = list(weight = "sum", function(x) length(x)))
-    
+
     # walktrap community detection using weights
     clus5 <- igraph::cluster_walktrap(gcon, weights = igraph::E(gcon)$weight)
     nodes$idgrp <- igraph::membership(clus5)
-    
+
     coords <- igraph::layout.fruchterman.reingold(gcon)
-    
+
     layout(matrix(c(1, 2), 1), widths = c(0.85, 1))
     par(mar = c(1, 1, 1, 1))
-    
+
     ## Colour palette creation
     pal <- vec_col1[as.factor(nodes$Type)]
     pal2 <- sapply(pal, darken, 30)
     pal3 <- vec_col2
     ## Network plotting
-    plot0(range(coords[, 1]) + 0.25 * c(-1, 1), range(coords[, 2]) + 0.25 * c(-1, 
+    plot0(range(coords[, 1]) + 0.25 * c(-1, 1), range(coords[, 2]) + 0.25 * c(-1,
         1), asp = 1)
     for (i in 1:nrow(links)) {
         idf <- which(nodes$id == links$from[i])
         idt <- which(nodes$id == links$to[i])
-        # 
-        lines(coords[c(idf, idt), 1L], coords[c(idf, idt), 2L], col = "grey50", lwd = 0.75 * 
+        #
+        lines(coords[c(idf, idt), 1L], coords[c(idf, idt), 2L], col = "grey50", lwd = 0.75 *
             links$orig.wt[i])
     }
-    
+
+    par(mar = c(1, 1.5, 1, 1))
     for (i in 1:length(clus5)) {
         pts <- c(NULL, NULL)
         tmp <- clus5[[i]]
@@ -59,16 +60,16 @@ walktrap_dendro <- function(links, nodes, vec_col1 = c("#f1bf5f", "#00ac89", "#2
     }
     points(coords[, 1], coords[, 2], pch = 21, bg = pal, col = pal2, cex = 4, lwd = 2)
     text(coords[, 1], coords[, 2], labels = nodes$idnum)
-    
+
     # Dendrogram plotting
     clus5d <- stats::as.hclust(clus5)
     clus5d$labels <- paste0(nodes$idnum, ": ", nodes$Node.name)  #nodes$idnum
     clus5p <- ape::as.phylo(clus5d)
-    
+
     par(mar = c(1, 1, 1, 0))
-    
+
     pal4 <- pal3[nodes$idgrp[clus5p$edge[, 2]]]
     pal4[is.na(pal4)] <- "grey50"
     ape::plot.phylo(clus5p, cex = 0.82, label.offset = 0.25, edge.color = pal4, edge.width = 1.6)
-    
+
 }
